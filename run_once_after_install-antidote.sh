fd0ca257906fd1cbb3c041102398cbece4d60434
Creling <csly004@gmail.com>
2024-08-03 00:07:51 +0800

    fix: replace 'source' with dot.

--- run_once_after_install-antidote.sh
+++ run_once_after_install-antidote.sh
@@ -2,9 +2,9 @@
 
 git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote
 
-source ~/.zshenv
-source ${ZDOTDIR:-~}/.zshrc
+. ~/.zshenv
+. ${ZDOTDIR:-~}/.zshrc
 
 antidote load
 
-source ${ZDOTDIR:-~}/.zshrc
+. ${ZDOTDIR:-~}/.zshrc
